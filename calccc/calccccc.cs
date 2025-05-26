4548c0c19158ccb68dabc2a3da50d98e75468a56
sasageyoas <sinsanyo@gmail.com>
2025-05-26 17:24:51 +0300

    Add files via upload

---
+++ calccc/calccccc.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcccc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a, b, res;
+                string c;
+                Console.Write("Введите первое число: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите второе число: ");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите знак: ");
+                c = Console.ReadLine();
+                if (c == "+")
+                {
+                    res = a + b;
+                    Console.Write($"Результат = {res}");
+                }
+                else if (c == "-")
+                {
+                    res = a - b;
+                    Console.Write($"Результат = {res}");
+                }
+                else if (c == "*")
+                {
+                    res = a * b;
+                    Console.Write($"Результат = {res}");
+                }
+                else if (c == "/")
+                {
+                    res = a / b;
+                    Console.Write($"Результат = {res}");
+                }
+                else if (c == "%")
+                {
+                    res = a / b;
+                    Console.Write($"Результат = {res}");
+                }
+                else if (c == "&")
+                {
+                    res = a & b;
+                    Console.Write($"Результат = {res}");
+                }
+                else if (c == "|")
+                {
+                    res = a | b;
+                    Console.Write($"Результат = {res}");
+                }
+                else if (c == "^")
+                {
+                    res = a ^ b;
+                    Console.Write($"Результат = {res}");
+                }
+                else
+                {
+                    Console.WriteLine("нинини!!! так нельзя!! атата!!");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("нинини!!! бить больна неправильное значение!!");
+            }
+        }
+    }
+}
